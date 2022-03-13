b62544d5034296e345613c93079251cae6d20759
Magali Garcia <60717025+magaligarcia@users.noreply.github.com>
2022-03-13 14:49:18 -0300

    Add files via upload

---
+++ Qué se implementó en ES7/codigo.js
@@ -0,0 +1,19 @@
+// clase #6 Hablemos de Ecmascript 7
+//definiremos un array
+
+let numbers = [1,2,3,7,8];
+if (numbers.includes (7)) { console.log('Si se incluye el valor 7');
+} else {
+console.log('No se encuentra.')
+}
+
+//se incluyo el método includes, en este caso en particular nos p ermite saber si un atributo se encuentra dentro del array. 
+//También se puede utilizar con strings
+//Cómo elevar una potencia?
+
+let base= 4;
+let exponent =3;
+let result = base ** exponent;
+console.log(result);
+
+//el uso del ** se añadio en esta versión de ecmascript
