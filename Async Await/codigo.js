045e79edd481e4d9bf9c5933141df3c9431767f6
Magali Garcia <60717025+magaligarcia@users.noreply.github.com>
2022-03-13 17:20:15 -0300

    Add files via upload

---
+++ Async Await/codigo.js
@@ -0,0 +1,30 @@
+
+const helloWorld =() =>{
+    return new Promise((resolve,reject)=>{
+        (true)
+        ? setTimeout(()=>resolve('helloWorld'),3000)
+        : reject(new Error ('Test Error'))
+    })
+}
+
+const helloAsync =async() =>{
+    const hello = await helloWorld();
+    console.log(hello);
+
+}
+helloAsync();
+
+//Nos permitirá usar trycatch y trabajar los errores correctamente.
+
+const another =async() =>{
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+
+    } catch (error){
+        console.log(error);
+        
+    }
+}
+
+another();
